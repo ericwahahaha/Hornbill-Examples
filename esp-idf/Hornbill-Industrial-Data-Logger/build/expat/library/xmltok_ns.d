2481591c568ffad96563932a21a37f731a2aecc6
Unknown <ericwahahaha@gmail.com>
2018-01-03 21:11:33 +0800

    Delete unnecessary files
    As title

--- esp-idf/Hornbill-Industrial-Data-Logger/build/expat/library/xmltok_ns.d
+++
@@ -1,2 +0,0 @@
-library/xmltok_ns.o: \
- F:/GitHub/Wireless/esp-idf/components/expat/library/xmltok_ns.c
--- esp-idf/Hornbill-Industrial-Data-Logger/build/jsmn/src/jsmn.d
+++
@@ -1,4 +0,0 @@
-src//jsmn.o: F:/GitHub/Wireless/esp-idf/components/jsmn/src/jsmn.c \
- F:/GitHub/Wireless/esp-idf/components/jsmn/include/jsmn.h
-
-F:/GitHub/Wireless/esp-idf/components/jsmn/include/jsmn.h:
